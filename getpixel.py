7f19e040f44b94cf299c8bee9a29fd6f726af4cd
Cody Young <cyoung1623@gmail.com>
2018-11-08 20:28:40 -0800

    Delete getpixel.py

--- getpixel.py
+++
@@ -1,8 +0,0 @@
-def getPixelsofRange():
-  filename = pickAFile()
-  pic = makePicture(filename)
-  for i in range(0, 5):
-    for j in range(0, 5):
-      pix = getPixel(pic, i, j)
-      print(pix)
-      
